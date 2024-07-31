9a69d6024d883de503cd72c7f23ac3996a108295
douglas <douglasmaxton@gmail.com>
2024-07-31 23:23:48 +0100

    feat: added swagger docs to creating user endpoint

--- app/api/user/create/route.js
+++ app/api/user/create/route.js
@@ -2,6 +2,44 @@ import { amount } from "@/utils/API";
 import prisma from "@/lib/prisma";
 import { response } from "@/utils/API/response";
 
+/**
+ * @swagger
+ * /api/user/create:
+ *  post:
+ *    tags: [Users]
+ *    summary: Creates a number of users in database
+ *    description: Returns message, count, length and status
+ *    parameters:
+ *      - in: query
+ *        name: amount
+ *        required: false
+ *        default: 10
+ *        description: Creates the number of users in db that you specify
+ *    responses:
+ *      200:
+ *        description: users' information
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  description: Message on success
+ *                  example: You have successfully created users in db
+ *                count:
+ *                  type: number
+ *                  description: Counts how many users are in database
+ *                  example: 10
+ *                length:
+ *                  type: number
+ *                  description: Shows the amount of users that were just created
+ *                  example: 2
+ *                status:
+ *                  type: number
+ *                  description: Shows that creation was successfully
+ *                  example: 200
+ */
 export async function POST(requests) {
   const { searchParams } = new URL(requests.url);
 
@@ -10,7 +48,7 @@ export async function POST(requests) {
   // for loop to create users based on query amount specified
   for (let i = 0; i < amount(searchParams); i++) {
     user.push({
-      name: `user${i}`,
+      name: `user${parseInt(await prisma.user.count()) + i}`,
     });
   }
 
@@ -19,15 +57,13 @@ export async function POST(requests) {
     data: user,
   });
 
-  // counts records in a database
   const count = await prisma.user.count();
-  // counts the length of users array
   const length = user.length;
 
   return response({
     message: "You have successfully created users in db",
-    count,
-    length,
+    "users in database": count,
+    "created users": length,
     status: 200,
   });
 }
