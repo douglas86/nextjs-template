eec6f8b24867a316af44a551d370ec3b1931f09c
douglas <douglasmaxton@gmail.com>
2024-08-19 16:38:39 +0100

    feat: reordered objects in next auth config

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,29 +4,36 @@ import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "database",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    encryption: false,
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+    async signIn() {
+      return true;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_APP_GOOGLE_CLIENT_ID,
       clientSecret: process.env.NEXT_APP_GOOGLE_SECRET_KEY,
     }),
   ],
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      console.log("user: ", user);
-      console.log("Account: ", account);
-      console.log("Profile: ", profile);
-      return true;
-    },
-    async session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "database",
-  },
-  // secret: process.env.NEXT_APP_SECRET_KEY,
 });
 
 export { handler as GET, handler as POST };
