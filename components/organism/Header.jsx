fc5c1ec60b008e9864d7521c21298f501794a14b
douglas <douglasmaxton@gmail.com>
2024-08-22 19:24:11 +0100

    feat: changed docker container to only use it for databases

--- components/organism/Header.jsx
+++ components/organism/Header.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 import { button } from "@/components/atom";
@@ -12,6 +13,28 @@ const Header = () => {
       {session
         ? button(() => signOut(), "btn", "Sign Out")
         : button(() => signIn(), "btn", "Sign In")}
+      <div className="max-w-md m-5 mx-auto overflow-hidden bg-white shadow-md rounded-xl md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:flex-shrink-0">
+            <Image
+              src={`/vercel.svg`}
+              alt="Vercel"
+              width={256}
+              height={256}
+              priority="high"
+              className="object-cover w-full h-48 md:w-48"
+            />
+          </div>
+          <div className="p-8">
+            <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
+              The cool store
+            </div>
+            <p className="mt-2 text-gray-500">
+              Visit our cool store and find the best products for you.
+            </p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
