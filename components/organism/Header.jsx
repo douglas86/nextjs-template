70c9f4fa6e26ca312509759a59ee52250305ef63
douglas <douglasmaxton@gmail.com>
2024-08-29 15:58:59 +0100

    feat: changed logged in icon to google profile image

--- components/organism/Header.jsx
+++ components/organism/Header.jsx
@@ -111,9 +111,7 @@ const Header = () => {
                       />
                     ) : (
                       <Image
-                        src={
-                          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        }
+                        src={session.user.image}
                         alt=""
                         className="h-8 w-8 rounded-full"
                         width={0}
