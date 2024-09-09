56dc181df3e01c1c5a8a7312ad37b60d6a9d475e
douglas <douglasmaxton@gmail.com>
2024-09-09 16:18:58 +0100

    fix: login and logout on users data stored to state store

--- components/organism/Header.jsx
+++ components/organism/Header.jsx
@@ -22,8 +22,8 @@ import useUser from "@/hooks/useUser";
 import useAppContext from "@/hooks/useAppContext";
 
 const Header = () => {
-  const user = useUser();
-  const { dispatch, user: s } = useAppContext();
+  // const user = useUser();
+  const { dispatch, user } = useAppContext();
   const [currentPage, setCurrentPage] = useState("");
 
   const { data: session } = useSession();
@@ -45,6 +45,8 @@ const Header = () => {
       dispatch({ type: "UPDATE_USER", payload: session.user });
     }
   }, [dispatch, session]);
+
+  console.log("s", user);
 
   return (
     <header className="header">
@@ -119,10 +121,10 @@ const Header = () => {
                   <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Open user menu</span>
-                    {user === null ? (
+                    {user ? (
                       <Image
-                        src={Customer}
-                        alt="No customer"
+                        src={user.image}
+                        alt=""
                         className="h-8 w-8 rounded-full"
                         width={0}
                         height={0}
@@ -130,8 +132,8 @@ const Header = () => {
                       />
                     ) : (
                       <Image
-                        src={user.image}
-                        alt=""
+                        src={Customer}
+                        alt="No customer"
                         className="h-8 w-8 rounded-full"
                         width={0}
                         height={0}
@@ -144,7 +146,16 @@ const Header = () => {
                   transition
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
-                  {user === null ? (
+                  {user ? (
+                    <MenuItem>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
+                      >
+                        <button onClick={() => signOut()}>Sign Out</button>
+                      </a>
+                    </MenuItem>
+                  ) : (
                     <MenuItem>
                       <a
                         href="#"
@@ -153,17 +164,6 @@ const Header = () => {
                         <button onClick={() => signIn()}>Sign In</button>
                       </a>
                     </MenuItem>
-                  ) : (
-                    <>
-                      <MenuItem>
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                        >
-                          <button onClick={() => signOut()}>Sign Out</button>
-                        </a>
-                      </MenuItem>
-                    </>
                   )}
                 </MenuItems>
               </Menu>
