44383f8a355f087c152439be1c4493a68848e896
douglas <douglasmaxton@gmail.com>
2024-09-13 16:08:10 +0100

    feat: added docstring and comments for GET request

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -4,21 +4,30 @@ import prisma from "@/lib/prisma";
 
 import { decryptData, skip, take, filter } from "@/utils/API";
 
+/**
+ * GET request for users data stored in database
+ * @param req
+ * @returns {Promise<NextResponse<{data: Awaited<unknown>[], length: *, message: string}>>}
+ * @constructor
+ */
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
 
+  // fetches number of documents in db
   const length = await prisma.user.count({
     where: {
       role: filter(searchParams, "user"),
     },
   });
 
+  // fetches users' data from db based
   const userData = await prisma.user.findMany({
     where: { role: filter(searchParams, "user") },
     skip: skip(searchParams),
     take: take(searchParams),
   });
 
+  // decrypts data from userData
   const data = await Promise.all(
     userData.map(async (items) => {
       return {
