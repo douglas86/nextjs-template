3c06a35f9dbbc1eca21d0b77a543f5f8bd0f6879
douglas <douglasmaxton@gmail.com>
2024-09-16 13:19:46 +0100

    feat: included swr hook for data fetching

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,22 @@
 
 import { spinner } from "@/components/atom";
 import useAppContext from "@/hooks/useAppContext";
+import useSWR from "swr";
 
 export default function Home() {
   const { user } = useAppContext();
+
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://api.github.com/repos/vercel/swr",
+    fetcher,
+  );
+
+  if (error) return "An error occurred";
+  if (isLoading) return "Loading...";
+
+  console.log("data", data);
 
   return (
     <main>
