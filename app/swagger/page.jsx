298de558f0502a8b91885d542514cdf4d3a3f8da
douglas <douglasmaxton@gmail.com>
2024-09-23 22:16:20 +0100

    feat: added logic to redirect is !user and !node

--- app/swagger/page.jsx
+++ app/swagger/page.jsx
@@ -1,19 +1,24 @@
 "use client";
 
 import SwaggerUI from "swagger-ui-react";
+import { redirect } from "next/navigation";
+
+import useAppContext from "@/hooks/useAppContext";
 
 import "swagger-ui-react/swagger-ui.css";
-import useAppContext from "@/hooks/useAppContext";
 
 const SwaggerPage = () => {
   const { user } = useAppContext();
 
+  // when site is in development then swagger can be displayed
   const node =
     process.env.NODE_ENV === "development" ? (
       <SwaggerUI url="/api/swagger" />
     ) : null;
 
-  return <>{user && node}</>;
+  // when a user not logged in or site in production
+  // gets redirected to landing page
+  return <>{user ? (node ? node : redirect("/")) : redirect("/")}</>;
 };
 
 export default SwaggerPage;
